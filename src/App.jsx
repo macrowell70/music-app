0751e2a0c43c30f9a43c36d6acbc8b34ef271526
unknown <macrowell@yahoo.com>
2022-11-22 13:33:04 -0500

    sets up routes for future pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './styles/global.css'
 import './styles/reset.css'
 import './App.css'
+import { 
+  BrowserRouter as Router, 
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import { Home, Setlist } from './components'
 
 function App() {
 
   return (
-    <div>music-app</div>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/set" element={<Setlist />}/>
+      </Routes>
   )
 }
 
---
+++ src/components/Setlist.jsx
@@ -0,0 +1,9 @@
+
+
+const Setlist = () => {
+  return (
+    <div>Setlist</div>
+  )
+}
+
+export default Setlist
